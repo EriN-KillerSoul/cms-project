ed2c7d933521b59250757014751336f455e571e2
anaxsnsra <anaxcool95@yahoo.com>
2017-06-17 09:46:29 +0800

    update user form worked

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EditUserRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\Role;
@@ -43,12 +44,18 @@ class AdminController extends Controller
     public function store(UsersRequest $request)
     {
         //
-        $input = $request->all();
+        if(trim($request->password) == ''){
+
+            $input = $request->except('password');
+        }
+        else{
+            $input = $request->all();
+        }
 
         if($file = $request->file('photo_id')){
 
             $name = time() . $file->getClientOriginalName();
-            $file->move('image', $name);
+            $file->move('images', $name);
             $photo = Photo::create(['file'=>$name]);
             $input['photo_id'] = $photo->id;
         }
@@ -80,6 +87,10 @@ class AdminController extends Controller
     public function edit($id)
     {
         //
+
+        $user = User::findOrFail($id);
+        $roles = Role::pluck('name','id')->all();
+        return view('admin.user.edit', compact('user','roles'));
     }
 
     /**
@@ -89,10 +100,31 @@ class AdminController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(EditUserRequest $request, $id)
     {
         //
-    }
+        $user = User::findOrFail($id);
+        if(trim($request->password) == ''){
+
+            $input = $request->except('password');
+        }
+        else{
+            $input = $request->all();
+        }
+        if($file = $request->file('photo_id')) {
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['file' => $name]);
+            $input['photo_id'] = $photo->id;
+        }
+            $input['password']= bcrypt($request->password);
+            $user->update($input);
+            return redirect('admin');
+     }
+
+
+
+
 
     /**
      * Remove the specified resource from storage.
---
+++ resources/views/admin/user/edit.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.admin')
+
+@section('section')
+
+    <h1>Edit User</h1>
+
+    <div class="row">
+
+    <div class="col sm-3">
+
+        <img height="300" width="300" src="{{asset($user->photo ? $user->photo->file : 'images/1.png')}}" alt="" class="img-responsive img-rounded">
+
+
+    </div>
+
+    {{--<form method="post" action="/post">--}}
+
+    <div class="col-sm-9">
+    {!! Form::model($user,['method'=>'PATCH','action'=>['AdminController@update', $user->id],'files'=>true]) !!}
+        <input type="hidden" name="_token" value="{!! csrf_token() !!}">
+        {{csrf_field()}}
+    <div class="form-group">
+        {!! Form::label('name','Name :') !!}
+        {!! Form::text('name', null, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('password','Password :') !!}
+        {!! Form::password('password', ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('email','Email :') !!}
+        {!! Form::text('email', null, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('role_id','Role :') !!}
+        {!! Form::select('role_id',$roles, null, ['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('is_active','Status :') !!}
+        {!! Form::select('is_active', array(1=>'Active',0=>'Not Active'), null, ['class'=>'form-control']) !!}
+    </div>
+    {{--<input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
+    {{--<input type="text" name="title" placeholder="enter your title">--}}
+    {{--<input type="submit" name="submit">--}}
+    <div class="form-group">
+        {!! Form::label('photo_id','Upload Your Picture') !!}
+        {!! Form::file('photo_id',null,['class'=>'form-control'])!!}
+    </div>
+
+
+    <div class="form-group">
+        {!! Form::submit('Update User',['class'=>'btn btn-primary']) !!}
+    </div>
+
+    </div>
+
+
+    {!! Form::close() !!}
+    </div>
+    <div class="row">
+    @include('include.error_form')
+    </div>
+@endsection
